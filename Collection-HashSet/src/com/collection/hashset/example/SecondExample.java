06302dada66dbb73afdd16cdf2a27d0e2fecb8a4
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-15 17:39:39 +0530

    set

---
+++ Collection-HashSet/src/com/collection/hashset/example/SecondExample.java
@@ -0,0 +1,13 @@
+package com.collection.hashset.example;
+
+public class SecondExample {
+
+	public static void main(String[] args) 
+	{
+		String s1 = "hello";
+		
+		System.out.println(s1.hashCode());
+		
+	}
+
+}
---
+++ Collection-HashSet/src/com/collection/hashset/example/SetExample.java
@@ -0,0 +1,22 @@
+package com.collection.hashset.example;
+
+import java.util.HashSet;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> city = new HashSet<String>();
+		
+		city.add("BLR");
+		city.add("HYD");
+		city.add("DEL");
+		city.add("LKO");
+		city.add("KNP");
+		
+		System.out.println(city);
+		
+		
+	}
+
+}
