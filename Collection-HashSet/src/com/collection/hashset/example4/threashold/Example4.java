ebe3111006e18f94b2aad35aef413cc025c5c51e
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-18 11:47:27 +0530

    fdsfa

---
+++ Collection-HashSet/src/com/collection/hashset/example4/threashold/Example4.java
@@ -0,0 +1,32 @@
+package com.collection.hashset.example4.threashold;
+
+import java.util.HashSet;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> prod =  new HashSet<String>();
+		
+		prod.add("FJWF");
+		prod.add("jvsdJ");
+		prod.add("kscs");
+		prod.add("wbff");
+		prod.add("sjkf");
+		prod.add("wfekb");
+		prod.add("fgfw");
+		prod.add("wfflw");
+		prod.add("fw");
+		prod.add("g2");
+		prod.add("wf");
+		prod.add("kd");
+		prod.add("ckjkf");
+		prod.add("lkna");
+		
+		
+		System.out.println(prod);
+		
+		
+	}
+
+}
