da33a70efb6ac0517327816b2fe344b3e8095b13
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-23 21:28:27 +0530

    fF

---
+++ Collection-HashSet/src/com/collection/linkedhashset/example13/Example13.java
@@ -0,0 +1,24 @@
+package com.collection.linkedhashset.example13;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Example13 {
+
+	public static void main(String[] args) 
+  	{
+		Set<String> patientName = new LinkedHashSet<String>();
+		
+		 patientName.add("nakul");
+		 patientName.add("keshav");
+		 patientName.add("dhanush");
+		 patientName.add("veer");
+		 patientName.add("shailza");
+		 
+		 
+		System.out.println(patientName);
+		
+
+	}
+
+}
